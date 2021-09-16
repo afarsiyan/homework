73a782645938f446ed953733cf71341ca9a1e16f
unknown <farsiyan.anahit@gmail.com>
2021-09-16 21:35:18 +0400

    could not do 2 out of 4 :(

---
+++ day_4_Anahit.py
@@ -0,0 +1,38 @@
+"""LOOPS"""
+
+# 1. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a nested loop.
+# Օգտագործելով ցիկլեր, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+
+
+# for i in range(1,10):
+
+ #չկարողացա սա կատարել։
+
+# 2. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a list comprehension.
+# Օգտագործելով comprehension, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+
+
+# lst_2 = [i*i for i in range(1,10) for [] in range ]
+# Չկարողացա մատրիցային տեսքով ներկայացնել :(
+
+
+
+# 4. Count the number of 'b's in the given string. DO NOT use the count() method.
+# Հաշվել տրված սթրինգում 'b'-երի քանակը։ Չօգտագործել count() մեթոդը։
+
+# nonsense = 'Blinking blindly, brainy Bob brings beautiful beer bottles beneath blue butterflies billowing brilliantly.'
+# result = int(0)
+# for i in range(len(nonsense)):
+#     if nonsense[i] == "b":
+#         result += 1
+# print(result)
+
+# 5. Write a program that will print the factorial of n.
+# Գրել ծրագիր, որը կտպի n թվի ֆակտորիալը։
+
+# number = int(3)
+# result = int(1)
+# for i in range(1, number + 1):
+#     result = result * i
+#
+# print(result)
